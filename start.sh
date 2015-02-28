08b145d7bce3e5bd57b84e63e9e5e4adc99eb109
root <root@static.226.155.76.144.clients.your-server.de>
2015-02-28 17:51:53 +0100

    updatethis

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 apt-get install screen
 apt-get install mysql-server mysql-client
 apt-get install python2.7-dev
+apt-get install python-pip
+pip install quark_hash
+apt-get install python-crypto
 apt-get install python-mysqldb
 screen -dmS bitcrystal-blockexplorer
 screen -S bitcrystal-blockexplorer -X stuff "python -m Abe.abe --config abe.conf"
